60d2b49e92afca33d26e0cb73572080784e65a67
Sheeladitya Rai <sheeladityarai@gmail.com>
2026-06-02 14:49:25 -0400

    Stage 3 completed:live prices implemented, along with open/closed positions, close_position function

--- tracker.py
+++ tracker.py
@@ -15,7 +15,7 @@ def calculate_pnl(trade):
     return (sell_price - trade["buy_price"]) * trade ["shares"]
 
 
-def add_trade(trades, ticker, buy_price, shares):
+def add_trade(trades, ticker, buy_price):
     #Bad ticker handling
     while True:
         price = yf.Ticker(ticker).fast_info["last_price"]
@@ -24,6 +24,16 @@ def add_trade(trades, ticker, buy_price, shares):
         else:
             print ("Invalid ticker")
             ticker = input("Ticker: ")
+    # No shares handling 
+    while True:
+        try:
+            shares = float(input("Shares: "))
+            if shares > 0:
+                break
+            else:
+                print ("You cannot have zero shares or negative shares.")
+        except ValueError:
+            print("Shares are numbers only")
     # Open/Close positions
     while True:
         status = input("Open/Closed Positon:" ).lower()
@@ -49,9 +59,53 @@ def view_trades(trades):
         # What trade I am working with
         number = i + 1
         pnl = calculate_pnl(trade)
-        print(f"Trade {number}: {trade["ticker"]} profit/loss ${pnl:.2f}. {trade["status"]}")
+        # need to handle sell price being none
+        if trade["sell_price"] is None:
+            sell_display = "Live"
+        else:
+            sell_display = f"${trade['sell_price']:.2f}"
+        # changed to show buy price/sell price
+        print(f"Trade {number}: {trade['ticker']} | Buy: ${trade['buy_price']:.2f} | Sell: {sell_display} | P&L: ${pnl:.2f} | {trade['status']}")
 
 #view_trades(trades)
+
+# Close open positions
+def close_position(trades):
+    open_trades = []
+    for index, trade in enumerate(trades):
+        if trade["status"] == "open":
+            open_trades.append((index, trade))
+    if open_trades:
+        for i, (index, trade) in enumerate(open_trades):
+            number = i +1
+            pnl = calculate_pnl(trade)
+            print (f"Trade {number}: {trade["ticker"]} profit/loss ${pnl:.2f}. {trade["status"]}")
+        #Error handling
+        while True:
+            try: 
+                choice = int(input("Enter a number: "))
+                if 1 <= choice <= len(open_trades):
+                    break
+                else: 
+                    print("Number out of range.")
+            except ValueError:
+                print ("Choice must be a number. ")
+        # Trying to find an open position and then closing it
+        while True: 
+            try:    
+                sell_price = float(input("Enter a sell price: "))
+                break
+            except ValueError:
+                    print("Sell price must be numbers.")
+        selected = open_trades[choice - 1]
+        original_index, _ = selected
+        trades [original_index]["sell_price"] = sell_price
+        trades [original_index] ["status"] = "closed"   
+    else:
+        print("No open trades.")
+
+
+        
 
 
 def show_summary(trades):
@@ -90,7 +144,9 @@ def display_menu():
         print('1. Add trade')
         print('2. View trades')
         print('3. Show summary')
-        print('4. Quit')
+        print('4. Close position')
+        print('5. Quit')
+       
 
 #display_menu()
 
@@ -129,17 +185,19 @@ while True:
      if choice == 1:
         ticker = input("Ticker: ")
         buy_price = float(input("Buy price: "))
-        shares = int(input("Shares: "))
-        add_trade(trades, ticker, buy_price, shares)
+        add_trade(trades, ticker, buy_price)
         save_trades(trades)
      elif choice == 2:
          view_trades(trades)
      elif choice == 3:
          show_summary(trades)
      elif choice == 4:
+         close_position(trades)
+         save_trades(trades)
+     elif choice == 5:
          break
      else:
-         print("Invalid choice. Pick between 1-4")
+         print("Invalid choice. Pick between 1-5")
 
     
 
