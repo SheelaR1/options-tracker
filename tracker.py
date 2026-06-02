e8f4975fe09283dcc369c6e440576ec384f90fac
SheelaR1 <159090876+SheelaR1@users.noreply.github.com>
2026-06-01 23:34:21 -0400

    Stage 2: add file persistence with JSON. Removed some uncessary code/comment

--- tracker.py
+++ tracker.py
@@ -1,27 +1,17 @@
+import json 
+
 # trading function running offline rn 
 
 def calculate_pnl(trade):
     #Returns the profit/loss: (sell_price - buy_price) × shares * just set it in a that it can be used later
     return (trade["sell_price"] - trade["buy_price"]) * trade ["shares"]
 
-# sample = {"ticker": "AAPL", "buy_price": 140.0, "sell_price": 175.0, "shares": 10}
-# print(calculate_pnl(sample))   # should print 250.0
-
-# loss_sample = {"ticker": "TSLA", "buy_price": 200.0, "sell_price": 180.0, "shares": 5}
-# print(calculate_pnl(loss_sample))   # should print -100.0
 
 def add_trade(trades, ticker, buy_price, sell_price, shares):
     # Build a new trade dict from the inputs
     new_trade = { "ticker": ticker, "buy_price":buy_price, "sell_price": sell_price, "shares":shares}
     # Append to the trades list
     trades.append(new_trade)
-
-
-trades = []   # start with empty list
-
-add_trade(trades, "AAPL", 150.0, 175.0, 10)
-add_trade(trades, "TSLA", 200.0, 180.0, 5)
-
 
 
 def view_trades(trades):
@@ -81,6 +71,22 @@ def get_user_choice():
 
 #get_user_choice()
 
+
+# save trades from memory 
+def save_trades(trades):
+    with open("trades.json", 'w') as f:
+        json.dump(trades, f)
+
+# load trades from the file 
+def load_trades():
+    try:
+        with open("trades.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+trades = load_trades()   # changed to load_trades function for stage 2
+1
 while True:
      display_menu()
      choice = get_user_choice()
@@ -91,6 +97,7 @@ while True:
         sell_price = float(input("Sell price: "))
         shares = int(input("Shares: "))
         add_trade(trades, ticker, buy_price, sell_price, shares)
+        save_trades(trades)
      elif choice == 2:
          view_trades(trades)
      elif choice == 3:
